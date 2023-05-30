59b4a44a57b40981cb9f66286506c5262ea10f22
Tavo Cruz <madecruz2@gmail.com>
2023-05-29 22:27:26 -0700

    new pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register/Register';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 function App() {
   return (
-    <div className="app">
-    oo
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
