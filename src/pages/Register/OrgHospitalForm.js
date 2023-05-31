e38a8cc12e3b654c763dd8093e1ff2d50812fcb2
Tavo Cruz <madecruz2@gmail.com>
2023-05-30 20:46:33 -0700

    add new registration functionality

--- src/pages/Register/OrgHospitalForm.js
+++ src/pages/Register/OrgHospitalForm.js
@@ -1,9 +1,30 @@
+import { Form, Input } from 'antd'
 import React from 'react'
 
-const OrgHospitalForm = ({type}) => {
-  return (
-    <div>OrgHospitalForm</div>
-  )
-}
+const OrgHospitalForm = ({type}) => (
+    <>
+        <Form.Item
+            label={type === "hospital" ? "Hospital Name" : "Organization Name"}
+            name={type==="hospital" ? "hospitalName" : "organizationName"}
+        >
+            <Input />
+        </Form.Item>
+        <Form.Item name="1tav0" label="1tav0">
+            <Input />
+        </Form.Item>
+        <Form.Item name="email" label="Email">
+            <Input />
+        </Form.Item>
+        <Form.Item name="phone" label="Phone">
+            <Input />
+        </Form.Item>
+        <Form.Item name="website" label="Website">
+            <Input />
+        </Form.Item>
+        <Form.Item name="password" label="Password">
+            <Input />
+        </Form.Item>
+    </>
+)
 
 export default OrgHospitalForm
