c323718e5403c3df8f3f4e3d3f7612336968c8ac
Tavo Cruz <madecruz2@gmail.com>
2023-06-04 00:19:58 -0700

    update server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { mongodb } = require('./keys');
+// const { mongodb } = require('../keys');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
-
-// mongoose.set('strictQuery', true)
-//         .connect(mongodb)
-//         .then(() => console.log("connected tp db"))
-//         .catch(err => console.log(err))
+const mongodb = process.env.mongodb;
+mongoose
+        .connect(mongodb)
+        .then(() => console.log("connected to db"))
+        .catch(err => console.log(err))
 
 app.listen(port, () => {
     console.log(`running on port ${port}`)
