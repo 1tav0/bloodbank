ad1661189096d8cda4975e068b9a82b511a3e9bb
Tavo Cruz <madecruz2@gmail.com>
2023-06-07 21:19:31 -0700

    Inventory Backend: Inventory Model

---
+++ server/models/invetoryModal.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const inventorySchema = new mongoose.Schema(
+    {
+        inventoryType: {
+            type: String,
+            required: true,
+            enum: ["in", "out"]
+        },
+        bloodGroup: {
+            type: String,
+            required: true,
+            enum: ["A+", "A-", "B+", "B-", "AB+", "AB-","O+", "O-"]
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        organization: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "organizations",
+            required: true
+        },
+        //if inventoryType is "out" then hospital will be set
+        //if inventoryType is "in" then donar will be set
+        hospital: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "users",
+            required: function () {
+                return this.inventoryTypr === "out"
+            }
+        },
+        donar: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "users",
+            required: function () {
+                return this.inventoryTypr === "in"
+            }
+        }
+    }, {
+        timestamps: true
+    }
+)
+
+const Inventory = mongoose.model("inventories", inventorySchema);
+module.exports = Inventory;
