7171c8621d51e3d7abb9037fd0f6647f0685c512
Tavo Cruz <madecruz2@gmail.com>
2023-06-20 21:06:36 -0700

    Organization Dashboard API

--- server/server.js
+++ server/server.js
@@ -8,9 +8,11 @@ const dbConfig = require('./config/dbConfig');
 //to make use of userRoutes
 app.use(express.json());
 const usersRoute = require("./routes/usersRoute") //entry point in the server 
-const inventoryRoute =  require('./routes/inventoryRoute')
+const inventoryRoute = require('./routes/inventoryRoute')
+const dashboardRoute = require("./routes/dashboardRoute")
 app.use('/api/users', usersRoute)
 app.use('/api/inventory', inventoryRoute)
+app.use('/api/dashboard', dashboardRoute)
 
 app.listen(port, () => {
     console.log(`running on port ${port}`)
