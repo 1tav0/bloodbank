bfb936d0f96efb3d377329dbc3e12e7ba017161a
Tavo Cruz <madecruz2@gmail.com>
2023-06-22 20:33:43 -0700

    donars & hospitals dashboards ui

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -38,48 +38,68 @@ const Home = () => {
   ]
   return (
     <div>
-      <span className="text-gray-700 text-2xl font-semibold">
-        Welcome {getLoggedInUserName(currentUser)}
-      </span>
-      <div className="grid grid-cols-4 gap-5 mb-5 mt-2">
-        {
-          bloodGroupData.map((bloodGroup, index) => {
-            const color = colors[index];
-            return (
-              <div
-                className={`p-5 flex justify-between text-white rounded items-center`}
-                style={
-                  {backgroundColor: color}
-                }
-              >
-                <h1 className='text-4xl uppercase'>{bloodGroup.bloodGroup}</h1>
-                <div className='flex flex-col justify-between gap-2'>
-                  <div className="flex justify-between gap-5">
-                    <span>Total In: </span>
-                    <span>{bloodGroup.totalIn} ML</span>
-                  </div>
+      {
+        currentUser.userType === "organization" &&
+        <>
+          <div className="grid grid-cols-4 gap-5 mb-5 mt-2">
+            {
+              bloodGroupData.map((bloodGroup, index) => {
+                const color = colors[index];
+                return (
+                  <div
+                    className={`p-5 flex justify-between text-white rounded items-center`}
+                    style={
+                      {backgroundColor: color}
+                    }
+                  >
+                    <h1 className='text-4xl uppercase'>{bloodGroup.bloodGroup}</h1>
+                    <div className='flex flex-col justify-between gap-2'>
+                      <div className="flex justify-between gap-5">
+                        <span>Total In: </span>
+                        <span>{bloodGroup.totalIn} ML</span>
+                      </div>
 
-                  <div className="flex justify-between">
-                    <span>Total Out: </span>
-                    <span>{bloodGroup.totalOut} ML </span>
-                  </div>
+                      <div className="flex justify-between">
+                        <span>Total Out: </span>
+                        <span>{bloodGroup.totalOut} ML </span>
+                      </div>
 
-                  <div className="flex justify-between">
-                    <span>Available: </span>
-                    <span>{bloodGroup.available} ML</span>
-                  </div>
-                </div>
+                      <div className="flex justify-between">
+                        <span>Available: </span>
+                        <span>{bloodGroup.available} ML</span>
+                      </div>
+                    </div>
 
-              </div>
-            )
-            
-          })
-        }
-      </div>
-      <span className="text-xl text-gray-700 font-semibold">
-        Your Recent Inventory
-      </span>
-      <InventoryTable filters={{ organization: currentUser._id }} limit={5}  userType={currentUser.userType}/>
+                  </div>
+                )
+                
+              })
+            }
+          </div>
+          <span className="text-xl text-gray-700 font-semibold">
+            Your Recent Inventory
+          </span>
+          <InventoryTable filters={{ organization: currentUser._id }} limit={5}  userType={currentUser.userType}/>
+        </>
+      }
+      {
+        currentUser.userType === "donar" &&
+        <div>
+          <span className="text-xl text-gray-700 font-semibold mt-5">
+            Your Recent Donations
+          </span>
+          <InventoryTable filters={{ donar: currentUser._id }} limit={5}  userType={currentUser.userType}/>
+        </div>
+      }
+      {
+        currentUser.userType === "hospital" &&
+        <div>
+          <span className="text-xl text-gray-700 font-semibold mt-5">
+            Your Recent Requests/Consumption
+          </span>
+          <InventoryTable filters={{ hospital: currentUser._id }} limit={5}  userType={currentUser.userType}/>
+        </div>
+      }
     </div>
   )
 }
